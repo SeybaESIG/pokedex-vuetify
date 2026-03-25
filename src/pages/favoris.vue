cb4fcdbe20739c1e98a4b4d73a48ca2414a9eb59
Seyba <mamadouseyba.tandia@divcom.ch>
2026-03-25 10:17:10 +0100

    feat: pages favoris et à propos + navigation

---
+++ src/pages/favoris.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Page affichant les Pokémon favoris de l'utilisateur -->
+  <v-container>
+    <h1 class="text-h3 text-center my-6">
+      Mes favoris
+    </h1>
+    <v-alert
+      type="info"
+      variant="tonal"
+    >
+      Vous n'avez pas encore de Pokémon favoris.
+    </v-alert>
+  </v-container>
+</template>
